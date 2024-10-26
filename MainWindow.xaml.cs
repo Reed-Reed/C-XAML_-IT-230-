3207c41e32c15f79048fb0f34ba79797ab66f434
Reed-Reed <146047315+Reed-Reed@users.noreply.github.com>
2024-10-26 09:21:05 -0400

    Initial commit
    Added all C# files as well as documentation on strategy and reflection.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFRegisterStudent
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Course choice;
+        int credit = 0;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            Course course1 = new Course("IT 145");
+            Course course2 = new Course("IT 200");
+            Course course3 = new Course("IT 201");
+            Course course4 = new Course("IT 270");
+            Course course5 = new Course("IT 315");
+            Course course6 = new Course("IT 328");
+            Course course7 = new Course("IT 330");
+
+
+            this.comboBox.Items.Add(course1);
+            this.comboBox.Items.Add(course2);
+            this.comboBox.Items.Add(course3);
+            this.comboBox.Items.Add(course4);
+            this.comboBox.Items.Add(course5);
+            this.comboBox.Items.Add(course6);
+            this.comboBox.Items.Add(course7);
+
+
+            this.textBox.Text = "";
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            choice = (Course)(this.comboBox.SelectedItem);
+            
+            if (choice.IsRegisteredAlready()) // Cannot register for the same course > once
+            {
+                this.label3.Content = "Already Registered";
+            }
+            else if (credit > 8) { // Cannot register for more than 3 courses
+                this.label3.Content = "Maximum credit hours reached.";
+            }
+            else    // Registers for course and increments credit hours
+            {
+                choice.SetToRegistered();
+                this.listBox.Items.Add(choice);
+                this.label3.Content = "Registration successful";
+                credit += 3;
+                this.textBox.Text = Convert.ToString(credit);
+                
+            }
+        }
+    }
+}
